1c8371563e29d342ce999ba87d2ecb0a5a8e2ee0
RussB-626 <russblaine.626@gmail.com>
2025-08-22 03:31:55 +0000

    update code logic to match excercises code for future planning

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import *
 
 def main():
-    test = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(test)
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-  TEXT_PLAIN = "text (plain)"
-  BOLD_TEXT = "**Bold text**"
-  ITALIC_TEXT = "__Italic text__"
-  CODE_TEXT = "`Code text`"
-  STD_LINK = "[anchor text](url)"
-  IMG_LINK = "![alt text](url)"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode():
   def __init__(self, text, text_type, url):
@@ -15,13 +15,11 @@ class TextNode():
     self.url = url
 
   def __eq__(self, other):
-    if (
+    return (
       self.text == other.text
       and self.text_type == other.text_type
       and self.url == other.url
-    ):
-      return True
-    return False
+    )
   
   def __repr__(self):
-    return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
