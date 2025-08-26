4f7dd81b0a4c004ad3d81ecfdb54045fcc6bf481
RussB-626 <russblaine.626@gmail.com>
2025-08-26 19:11:25 +0000

    updated extraction methods to match course code for next lessons

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -22,7 +22,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new_nodes
 
 def extract_markdown_images(text):
-    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
 
 def extract_markdown_links(text):
-    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
