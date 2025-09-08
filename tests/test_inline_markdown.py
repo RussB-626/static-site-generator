e15ee23c54560621cc8bc50a8e2898b8c5cda898
RussB-626 <russblaine.626@gmail.com>
2025-09-08 16:26:35 +0000

    updated unit tests to mimic course code

--- tests/test_inline_markdown.py
+++ tests/test_inline_markdown.py
@@ -66,6 +66,19 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and _italic_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+        self.assertEqual(
+            [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+            ],
+            new_nodes,
+        )
+
     def test_delim_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -158,8 +171,6 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
-
-class TestTextToTextNodes(unittest.TestCase):
     def test_text_to_textnodes(self):
         text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
         text_nodes = text_to_textnodes(text)
