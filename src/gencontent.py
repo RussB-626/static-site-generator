fad5da15bdfdd590f539391c99e3f31138c32f83
RussB-626 <russblaine.626@gmail.com>
2025-09-15 21:01:19 +0000

    generate pages recursively with new added pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from src.markdown_blocks import markdown_to_html_node
 
 def generate_page(from_path, template_path, dest_path):
@@ -31,3 +32,19 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+    
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            file_path = Path(dest_path)
+            new_file_path = file_path.with_suffix(".html")
+            generate_page(from_path, template_path, new_file_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from src.copystatic import copy_files_recursive
-from src.gencontent import generate_page
+from src.gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -16,12 +16,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    print("Generating pages...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
     
 
 if __name__ == "__main__":
