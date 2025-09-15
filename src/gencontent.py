a85400757c794de2d1836345b720714c2b1785ab
RussB-626 <russblaine.626@gmail.com>
2025-09-15 21:04:58 +0000

    updated code to match chapter and lesson code

--- src/gencontent.py
+++ src/gencontent.py
@@ -35,16 +35,11 @@ def extract_title(md):
 
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
-    
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        print(f" * {from_path} -> {dest_path}")
         if os.path.isfile(from_path):
-            file_path = Path(dest_path)
-            new_file_path = file_path.with_suffix(".html")
-            generate_page(from_path, template_path, new_file_path)
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
         else:
             generate_pages_recursive(from_path, template_path, dest_path)
