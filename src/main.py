fe6dd392637cf9034f06a5e6a6fca4ef894f1587
RussB-626 <russblaine.626@gmail.com>
2025-09-15 16:33:05 +0000

    copy static directory into public directory function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,40 @@
-from textnode import *
+import os
+import shutil
+
 
 def main():
-    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(test)
+    copy_static_directory()
+
+def copy_static_directory():
+    current_dir = os.getcwd()
+    public_dir = os.path.join(current_dir,"public")
+    static_dir = os.path.join(current_dir,"static")
+
+    print(f"starting copy of: {static_dir} into {public_dir}...")
+    
+    # Create a clean Public directory
+    if os.path.exists(public_dir):
+        print(f"deleting: {public_dir}...")
+        shutil.rmtree(public_dir)    
+
+    recursive_copy(static_dir, public_dir)
+    print(f"finished copying: {static_dir} into {public_dir}.")
+
+def recursive_copy(source, destination):
+    print(f"creating: {destination}...")
+    os.mkdir(destination)
+
+    print(f"copying contents of directory: {source} into {destination}...")
+    dir_contents = os.listdir(source)
+    for dir_content in dir_contents:
+        source_content_path = os.path.join(source,dir_content)
+        destination_content_path = os.path.join(destination,dir_content)
+        if(os.path.isfile(source_content_path)):
+            print(f"copying file: {source_content_path} into {destination_content_path}")
+            shutil.copy(source_content_path, destination_content_path)
+        else:
+            recursive_copy(source_content_path, destination_content_path)
+    
 
 if __name__ == "__main__":
     main()
